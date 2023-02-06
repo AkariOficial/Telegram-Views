1a28fda47bc97ee30e20697457b7abeb7d947acc
Akari <akariondev@gmail.com>
2023-02-06 00:21:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def tui():
   [ Views ]: {real_views}
  
   [ Conexão Erros ]: {proxy_errors}
-  [ Token   Erros ]: {token_errors}
+  [ Token Erros ]: {token_errors}
  
   [ Threads ]: {active_count()}
         ''')
@@ -143,14 +143,14 @@ def tui():
 
 
 print(logo)
-channel, post = input(' [ INPUT ] Digite Link Post: ').replace('https://t.me/', '').split('/')
+channel, post = input(' [ INPUT ] Link da postagem: ').replace('https://t.me/', '').split('/')
 
 try:
     search('<span class="tgme_widget_message_views">([^<]+)', requests.get(f'https://t.me/{channel}/{post}',
     params={'embed': '1', 'mode': 'tme'}, headers={'referer': f'https://t.me/{channel}/{post}', 'user-agent': USER_AGENT}).text).group(1)
 except: print(' [ OUTPUT ] Error | Channel Or Post Not Found!');sleep(3);exit()
 else:
-    print(' [ OUTPUT ] declarado | Aguarde alguns segundos para executar os threads...')
+    print(' [ OUTPUT ] Aguarde alguns segundos para executar os threads...')
     Thread(target=start_view).start()
     Thread(target=check_views).start()
     sleep(7.5);system('cls' if name == 'nt' else 'clear')
